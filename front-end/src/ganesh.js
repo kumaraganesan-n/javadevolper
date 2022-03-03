4bf7324772f0af0af7f99d10fb408f908acf9f42
kumaraganesan-n <kumaraganesan2000@gmail.com>
2022-03-03 15:23:04 +0530

    hai

---
+++ front-end/src/ganesh.js
@@ -0,0 +1,18 @@
+import React form "react";
+
+
+function App()
+{
+    return(
+        <h1>React Basic Functional Componenet</h1>
+        
+    )
+}
+function Hey()
+(
+    return (
+        <p> ganesan</p>
+    )
+)
+export {App}
+export default Hey;
---
+++ front-end/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Hey, { App } from './ganesh';
+import { ganesan } from './kumaran';
+
+ReactDOM.render(
+  <>
+    <App/>
+    <Hey/>
+    <ganesan/>
+
+  </>
+  
+  ,document.getElementById("root")
+)
